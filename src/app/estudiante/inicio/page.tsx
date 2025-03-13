b50094073851a3010e3c88fa5e216976dace928f
Alejandro Grijalba <144760007+AlejandroGrijalba@users.noreply.github.com>
2025-03-12 23:36:24 -0500

    Arreglando conflictos (#6)
    * Interfaz inicio de sesion y registro
    
    * Arreglando conflictos
    
    * Arreglando conflictos
    
    * Arreglando conflictos
    
    * Update package.json
    
    * Arreglando conflictos Parte 2
    
    * Arreglando conflictos Parte 2

--- src/app/estudiante/inicio/page.tsx
+++ src/app/estudiante/inicio/page.tsx
@@ -2,8 +2,9 @@ export default function Inicio() {
   return (
     <div>
       <div className="bg-white" >
-        <h1 className="">Semillero Univalle</h1>
+        <h1 className="">Semillero Univalle - Estudiante</h1>
       </div>
+
       <div></div>
     </div>
   );
