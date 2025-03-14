5c76180549e8d1edbebccefd4a2360529680920a
Alejandro Grijalba <cesar.grijalba@correounivalle.edu.co>
2025-03-13 23:47:08 -0500

    Añadiendo cositas en inicio de estudiantes

--- src/app/estudiante/layout.tsx
+++ src/app/estudiante/layout.tsx
@@ -1,14 +1,15 @@
 import "../globals.css";
 import Image from "next/image";
-import Navbar from "../../../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="">
-      <div className="bg-white">
-        <h1 className="">Semillero Univalle - Estudiante</h1>
-        <Navbar></Navbar>
-      </div>
+        <div className="bg-white p-4"><h1 className="">Semillero Univalle</h1>
+        <div className=" flex justify-around items-center mx-auto w-3/4 rounded-2xl bg-[#e8e8e8] p-2">
+          <Navbar></Navbar>
+        </div></div>
+ 
       <div className="">{children}</div>
     </div>
   );
