82ba99c9649839d5946e19320eb7d6f9f1c41deb
Alejandro Grijalba <cesar.grijalba@correounivalle.edu.co>
2025-10-11 17:11:42 -0500

    Sidebar modificado

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,8 @@
 "use client";
 import "../globals.css";
 import { StyledEngineProvider } from "@mui/material/styles";
-import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
-
 import Sidebar from "../../components/Sidebar";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { API_BASE_URL } from "../../../config";
 import Footer from "@/components/Footer";
-import Image from "next/image";
 
 
 export default function LayoutAdmin({
@@ -16,57 +10,11 @@ export default function LayoutAdmin({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter(); // Hook para redirigir
 
-  const handleLogout = async () => {
-    try {
-      // Realiza la solicitud de logout
-      await axios.post(
-        `${API_BASE_URL}/logout/`,
-        {},
-        {
-          headers: {
-            Authorization: `Token ${localStorage.getItem("token")}`,
-          },
-        },
-      );
-
-      // Limpia el token del localStorage
-      localStorage.removeItem("token");
-
-      // Redirige al usuario a la página de login
-      router.push("/auth/login");
-    } catch (error) {
-      console.error("Error al cerrar sesión:", error);
-      alert("Hubo un problema al cerrar sesión. Inténtalo de nuevo.");
-    }
-  };
   return (
     <StyledEngineProvider injectFirst>
       <div className="flex flex-row">
-        <div className="sticky top-0 h-screen w-1/5 bg-white p-4">
-
-          <Image
-            className="m-4"
-            src="/logoGrisSemillero.png"
-            alt="Logo Semillero"
-            width={250}
-            height={85}
-            priority
-          />
-          <div className="mx-auto mt-2 flex w-full flex-col justify-around">
-            <Sidebar></Sidebar>
-          </div>
-
-          <button
-            onClick={handleLogout} // Llama a la función mejorada
-            className="text-md my-1 flex w-full grow items-center justify-center gap-4 rounded-lg p-3 font-medium text-[#575757] hover:bg-[#C20E1A] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            {/* <span className="material-symbols-outlined">logout</span> */}
-            <ArrowRightOnRectangleIcon className="w-5" />
-            <p className="hidden font-bold md:block">Cerrar Sesión</p>
-          </button>
-        </div>
+        <Sidebar />
         <div id="container" className="w-4/5 p-5">
           {children}
         </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,14 +6,20 @@ import {
   HomeIcon,
   BookOpenIcon,
   CogIcon,
+  ArrowRightOnRectangleIcon
 } from "@heroicons/react/24/outline";
 import GroupWorkOutlinedIcon from "@mui/icons-material/GroupWorkOutlined";
 import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
-import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
+import LocalLibraryOutlinedIcon from "@mui/icons-material/LocalLibraryOutlined";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { API_BASE_URL } from "../../config";
 
 type LinkItem = {
   name: string;
@@ -46,7 +52,7 @@ const adminLinks = [
         href: "/admin/oferta/verOfertas",
         icon: TagIcon,
       },
-      {name: "Grupos", href: "/admin/grupos/ver-grupos", icon: UsersIcon},
+      { name: "Grupos", href: "/admin/grupos/ver-grupos", icon: UsersIcon },
     ],
   },
 
@@ -61,9 +67,35 @@ const profesorLinks = [
 ];
 
 export default function NavLinks() {
+  const router = useRouter(); // Hook para redirigir
+
   const [openDropdown, setOpenDropdown] = useState<string | null>(null);
   const pathname = usePathname();
   const [tipoUsuario, setTipoUsuario] = useState(null);
+
+  const handleLogout = async () => {
+    try {
+      // Realiza la solicitud de logout
+      await axios.post(
+        `${API_BASE_URL}/logout/`,
+        {},
+        {
+          headers: {
+            Authorization: `Token ${localStorage.getItem("token")}`,
+          },
+        },
+      );
+
+      // Limpia el token del localStorage
+      localStorage.removeItem("token");
+
+      // Redirige al usuario a la página de login
+      router.push("/auth/login");
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+      alert("Hubo un problema al cerrar sesión. Inténtalo de nuevo.");
+    }
+  };
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -87,58 +119,84 @@ export default function NavLinks() {
   }
 
   return (
-    <nav>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        const hasChildren = !!link.children;
+    <div className="sticky top-0 h-screen w-1/5 bg-white p-4">
+      <Image
+        className="m-4"
+        src="/logoGrisSemillero.png"
+        alt="Logo Semillero"
+        width={250}
+        height={85}
+        priority
+      />
 
-        return (
-          <div key={link.name}>
-            {hasChildren ? (
-              <div
-                className={`text-md my-1 flex grow items-center gap-4 rounded-lg p-3 font-medium text-[#575757] hover:bg-[#C20E1A] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3 cursor-pointer ${
-                  openDropdown === link.name ? "bg-[#C20E1A] text-gray-50" : ""
-                }`}
-                onClick={() =>
-                  setOpenDropdown(openDropdown === link.name ? null : link.name)
-                }
-              >
-                <LinkIcon className="w-5" />
-                <p className="hidden font-bold md:block">{link.name}</p>
-                <span className="ml-auto">
-                  {openDropdown === link.name ? "▲" : "▼"}
-                </span>
-              </div>
-            ) : (
-              <Link
-                href={link.href!}
-                className={`text-md my-1 flex grow items-center gap-4 rounded-lg p-3 font-medium text-[#575757] hover:bg-[#C20E1A] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3 ${
-                  pathname === link.href ? "bg-[#C20E1A] text-gray-50" : ""
-                }`}
-              >
-                <LinkIcon className="w-5" />
-                <p className="hidden font-bold md:block">{link.name}</p>
-              </Link>
-            )}
-            {hasChildren && openDropdown === link.name && (
-              <div className="ml-8 flex flex-col">
-                {link.children!.map((child) => (
-                  <Link
-                    key={child.name}
-                    href={child.href!}
-                    className={`my-1 flex items-center gap-2 rounded px-2 py-1 text-md text-[#575757] hover:bg-[#C20E1A] hover:text-white ${
-                      pathname === child.href ? "bg-[#C20E1A] text-gray-50" : ""
-                    }`}
-                  >
-                    <child.icon className="w-4" />
-                    {child.name}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        );
-      })}
-    </nav>
+      <nav>
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          const hasChildren = !!link.children;
+
+          return (
+            <div key={link.name}>
+              {hasChildren ? (
+                <div
+                  className={`text-md my-1 flex grow cursor-pointer items-center gap-4 rounded-lg p-3 font-medium text-[#575757] hover:bg-[#C20E1A] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3 ${
+                    openDropdown === link.name
+                      ? "bg-[#C20E1A] text-gray-50"
+                      : ""
+                  }`}
+                  onClick={() =>
+                    setOpenDropdown(
+                      openDropdown === link.name ? null : link.name,
+                    )
+                  }
+                >
+                  <LinkIcon className="w-5" />
+                  <p className="hidden font-bold md:block">{link.name}</p>
+                  <span className="ml-auto">
+                    {openDropdown === link.name ? "▲" : "▼"}
+                  </span>
+                </div>
+              ) : (
+                <Link
+                  href={link.href!}
+                  className={`text-md my-1 flex grow items-center gap-4 rounded-lg p-3 font-medium text-[#575757] hover:bg-[#C20E1A] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3 ${
+                    pathname === link.href ? "bg-[#C20E1A] text-gray-50" : ""
+                  }`}
+                >
+                  <LinkIcon className="w-5" />
+                  <p className="hidden font-bold md:block">{link.name}</p>
+                </Link>
+              )}
+              {hasChildren && openDropdown === link.name && (
+                <div className="ml-8 flex flex-col">
+                  {link.children!.map((child) => (
+                    <Link
+                      key={child.name}
+                      href={child.href!}
+                      className={`text-md my-1 flex items-center gap-2 rounded px-2 py-1 text-[#575757] hover:bg-[#C20E1A] hover:text-white ${
+                        pathname === child.href
+                          ? "bg-[#C20E1A] text-gray-50"
+                          : ""
+                      }`}
+                    >
+                      <child.icon className="w-4" />
+                      {child.name}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </nav>
+
+      <button
+        onClick={handleLogout} // Llama a la función mejorada
+        className="text-md my-1 flex w-full grow items-center justify-center gap-4 rounded-lg p-3 font-medium text-[#575757] hover:bg-[#C20E1A] hover:text-white md:flex-none md:justify-start md:p-2 md:px-3"
+      >
+        {/* <span className="material-symbols-outlined">logout</span> */}
+        <ArrowRightOnRectangleIcon className="w-5" />
+        <p className="hidden font-bold md:block">Cerrar Sesión</p>
+      </button>
+    </div>
   );
 }
