0fb443aade996e1c01ddf3e90bb28877b0935ba6
Alejandro Grijalba <144760007+AlejandroGrijalba@users.noreply.github.com>
2026-02-09 22:57:57 -0500

    Modulando (#46)
    * Modelando
    
    * modulando formulario
    
    * Reorganización del formulario
    
    * Arreglos 29 de enero
    
    * Modulando 30/01/2026
    
    * 31/01/26
    
    * modulando solucionado
    
    ---------
    
    Co-authored-by: Alejandro Grijalba <pendejandro@Alejandros-Mac-mini.local>

--- src/app/estudiante/perfil/page.tsx
+++ src/app/estudiante/perfil/page.tsx
@@ -1,9 +1,24 @@
 "use client";
 
+import { use, useEffect, useState } from "react";
 import FormularioUsuario from "@/components/formulario";
 
 export default function PerfilUsuario() {
-  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  interface User {
+    id: number;
+    token: string;
+    tipo_usuario: string;
+  }
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    // Aquí localStorage está disponible (lado del cliente)
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    setUser(user);
+  }, []);
+
+  if (!user) return <p>Cargando...</p>;
 
   return (
     <div className="mx-auto my-4 w-10/12 content-center">
