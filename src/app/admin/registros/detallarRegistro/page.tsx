0c27050fa6e27d13d670d6f614a28c944fe399a8
Alejandro Grijalba <pendejandro@Alejandros-Mac-mini.local>
2026-02-09 22:49:53 -0500

    modulando solucionado

--- src/app/admin/registros/detallarRegistro/page.tsx
+++ src/app/admin/registros/detallarRegistro/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { useEffect, useState } from 'react';
 import FormularioUsuario from "@/components/formulario";
 
 export default function PerfilUsuario() {
-  const user = JSON.parse(localStorage.getItem("user") || "{}");
+
+const [estudiante, setEstudiante] = useState(null);
+const [id_estudiante, setIdEstudiante] = useState(0);
+
+  useEffect(() => {
+    // Aquí localStorage está disponible (lado del cliente)
+
   const estudiante = JSON.parse(localStorage.getItem("inscritoSeleccionado") || "{}");
   const id_estudiante = estudiante?.id; // ✅ Usa optional chaining
+  setEstudiante(estudiante);
+  setIdEstudiante(id_estudiante);
+  }, []);
+
+
 
   return (
     <div className="mx-auto my-4 w-11/12 content-center">
